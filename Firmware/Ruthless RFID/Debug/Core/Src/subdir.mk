df07f7ae09448c1e8236fcb3fe13b8d7cc39a935
TheZ0 <rhett.humphreys1@gmail.com>
2024-06-30 20:13:38 +1000

    Enabled echoing of USB data

--- Firmware/Ruthless RFID/Debug/Core/Src/subdir.mk
+++ Firmware/Ruthless RFID/Debug/Core/Src/subdir.mk
@@ -9,6 +9,7 @@ C_SRCS += \
 ../Core/Src/OLED.c \
 ../Core/Src/W25N01GVZEIG.c \
 ../Core/Src/button.c \
+../Core/Src/commands.c \
 ../Core/Src/freertos.c \
 ../Core/Src/main.c \
 ../Core/Src/ruthlessfs.c \
@@ -25,6 +26,7 @@ OBJS += \
 ./Core/Src/OLED.o \
 ./Core/Src/W25N01GVZEIG.o \
 ./Core/Src/button.o \
+./Core/Src/commands.o \
 ./Core/Src/freertos.o \
 ./Core/Src/main.o \
 ./Core/Src/ruthlessfs.o \
@@ -41,6 +43,7 @@ C_DEPS += \
 ./Core/Src/OLED.d \
 ./Core/Src/W25N01GVZEIG.d \
 ./Core/Src/button.d \
+./Core/Src/commands.d \
 ./Core/Src/freertos.d \
 ./Core/Src/main.d \
 ./Core/Src/ruthlessfs.d \
@@ -60,7 +63,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/MFRC.cyclo ./Core/Src/MFRC.d ./Core/Src/MFRC.o ./Core/Src/MFRC.su ./Core/Src/OLED.cyclo ./Core/Src/OLED.d ./Core/Src/OLED.o ./Core/Src/OLED.su ./Core/Src/W25N01GVZEIG.cyclo ./Core/Src/W25N01GVZEIG.d ./Core/Src/W25N01GVZEIG.o ./Core/Src/W25N01GVZEIG.su ./Core/Src/button.cyclo ./Core/Src/button.d ./Core/Src/button.o ./Core/Src/button.su ./Core/Src/freertos.cyclo ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/ruthlessfs.cyclo ./Core/Src/ruthlessfs.d ./Core/Src/ruthlessfs.o ./Core/Src/ruthlessfs.su ./Core/Src/screen.cyclo ./Core/Src/screen.d ./Core/Src/screen.o ./Core/Src/screen.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_hal_timebase_tim.cyclo ./Core/Src/stm32f4xx_hal_timebase_tim.d ./Core/Src/stm32f4xx_hal_timebase_tim.o ./Core/Src/stm32f4xx_hal_timebase_tim.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su
+	-$(RM) ./Core/Src/MFRC.cyclo ./Core/Src/MFRC.d ./Core/Src/MFRC.o ./Core/Src/MFRC.su ./Core/Src/OLED.cyclo ./Core/Src/OLED.d ./Core/Src/OLED.o ./Core/Src/OLED.su ./Core/Src/W25N01GVZEIG.cyclo ./Core/Src/W25N01GVZEIG.d ./Core/Src/W25N01GVZEIG.o ./Core/Src/W25N01GVZEIG.su ./Core/Src/button.cyclo ./Core/Src/button.d ./Core/Src/button.o ./Core/Src/button.su ./Core/Src/commands.cyclo ./Core/Src/commands.d ./Core/Src/commands.o ./Core/Src/commands.su ./Core/Src/freertos.cyclo ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/ruthlessfs.cyclo ./Core/Src/ruthlessfs.d ./Core/Src/ruthlessfs.o ./Core/Src/ruthlessfs.su ./Core/Src/screen.cyclo ./Core/Src/screen.d ./Core/Src/screen.o ./Core/Src/screen.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_hal_timebase_tim.cyclo ./Core/Src/stm32f4xx_hal_timebase_tim.d ./Core/Src/stm32f4xx_hal_timebase_tim.o ./Core/Src/stm32f4xx_hal_timebase_tim.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su
 
 .PHONY: clean-Core-2f-Src
 
